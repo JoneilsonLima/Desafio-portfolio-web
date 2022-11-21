574fb198c285c913950cb65780101d3bb34b5482
joneilson <joneilson84@gmail.com>
2022-11-21 11:54:22 -0300

    update js

--- js/index.js
+++ js/index.js
@@ -23,42 +23,52 @@ function initMenuMobile() {
 }
 initMenuMobile();
 
-/**** Modal Projeto 1 ****/
-const linkProjeto1 = document.querySelector('.link-projeto1');
-const projeto1 = document.querySelector('.projeto1');
-const closedModal = document.querySelectorAll('.closed-modal');
 
-function openProjeto1() {
-    projeto1.classList.add('display-flex-modal')
+/**** Início Modal *****/
+const bodyProjetos = document.querySelector('body');
 
-    closedModal[0].addEventListener('click', ()=> {
-        projeto1.classList.remove('display-flex-modal');
-    })
+if(bodyProjetos.classList.contains('body-projetos-modal')) {
+    function initModal() {
+        /**** Modal Projeto 1 ****/
+        const linkProjeto1 = document.querySelector('.link-projeto1');
+        const projeto1 = document.querySelector('.projeto1');
+        const closedModal = document.querySelectorAll('.closed-modal');
+    
+        function openProjeto1() {
+            projeto1.classList.add('display-flex-modal')
+        
+            closedModal[0].addEventListener('click', ()=> {
+                projeto1.classList.remove('display-flex-modal');
+            })
+        }
+        linkProjeto1.addEventListener('click', openProjeto1);
+        
+        /**** Modal Projeto 2 ****/
+        const linkProjeto2 = document.querySelector('.link-projeto2');
+        const projeto2 = document.querySelector('.projeto2');
+        
+        function openProjeto2() {
+            projeto2.classList.add('display-flex-modal')
+        
+            closedModal[1].addEventListener('click', ()=> {
+                projeto2.classList.remove('display-flex-modal')
+            })
+        }
+        linkProjeto2.addEventListener('click', openProjeto2);
+        
+        /**** Modal Projeto 3 ****/
+        const linkProjeto3 = document.querySelector('.link-projeto3');
+        const projeto3 = document.querySelector('.projeto3');
+        
+        function openProjeto3() {
+            projeto3.classList.add('display-flex-modal')
+        
+            closedModal[2].addEventListener('click', ()=> {
+                projeto3.classList.remove('display-flex-modal')
+            })
+        }
+        linkProjeto3.addEventListener('click', openProjeto3);
+    }
+    initModal();
 }
-linkProjeto1.addEventListener('click', openProjeto1);
 
-/**** Modal Projeto 2 ****/
-const linkProjeto2 = document.querySelector('.link-projeto2');
-const projeto2 = document.querySelector('.projeto2');
-
-function openProjeto2() {
-    projeto2.classList.add('display-flex-modal')
-
-    closedModal[1].addEventListener('click', ()=> {
-        projeto2.classList.remove('display-flex-modal')
-    })
-}
-linkProjeto2.addEventListener('click', openProjeto2);
-
-/**** Modal Projeto 3 ****/
-const linkProjeto3 = document.querySelector('.link-projeto3');
-const projeto3 = document.querySelector('.projeto3');
-
-function openProjeto3() {
-    projeto3.classList.add('display-flex-modal')
-
-    closedModal[2].addEventListener('click', ()=> {
-        projeto3.classList.remove('display-flex-modal')
-    })
-}
-linkProjeto3.addEventListener('click', openProjeto3);
