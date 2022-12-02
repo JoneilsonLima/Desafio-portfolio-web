b8d4631b30a0acf15d5c1f7df2d3c825a8842773
joneilson <joneilson84@gmail.com>
2022-12-02 17:29:38 -0300

    update area de mensagem

---
+++ js/mensagem.js
@@ -0,0 +1,60 @@
+let arr = [];
+function salvar() {
+    if (localStorage.listaMensagem) {
+        arr = JSON.parse(localStorage.getItem('listaMensagem'))
+    }
+
+    let nome = document.querySelector('#mensagem').value;
+    arr.push(nome);
+    nome = '';
+    localStorage.listaMensagem = JSON.stringify(arr);
+}
+
+function mostrar() {
+    let result = document.querySelector('.nova-msg');
+    result.innerHTML = '';
+    if (localStorage.listaMensagem) {
+        arr = JSON.parse(localStorage.getItem('listaMensagem'))
+    }
+
+    for (let i in arr) {
+        let p = document.createElement("li");
+        p.innerHTML = arr[i];
+        result.append(p);
+    }
+}
+
+function limpar() {
+    localStorage.clear();
+    window.location.reload(true);
+}
+
+function botaoHandler(event) {
+    event.preventDefault()
+    console.log("Botão Clicado!")
+}
+
+
+/*** MENSAGEM SALVA ***/
+function initAlertaMensagemSalva() {
+    const botaoEnviar = document.querySelector("#alerta-mensagem");
+    const divMessage = document.querySelector(".alert");
+
+    const msg = "Mensagem Salva";
+
+    function ativarMensagem(msg) {
+        const message = document.createElement('div');
+        message.classList.add('mensagem-salva')
+        message.innerText = msg;
+        divMessage.appendChild(message)
+
+        setTimeout(() => {
+            message.style.display = "none";
+        }, 3000);
+    }
+
+    botaoEnviar.addEventListener("click", () => {
+        ativarMensagem(msg);
+    });
+}
+initAlertaMensagemSalva()
